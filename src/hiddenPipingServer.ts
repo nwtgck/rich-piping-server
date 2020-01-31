64685a2f21ac1293a56178db280890e2b727b209
Ryo Ota <nwtgck@nwtgck.org>
2020-02-01 00:10:08 +0900

    support basic authentication

--- src/hiddenPipingServer.ts
+++ src/hiddenPipingServer.ts
@@ -2,6 +2,8 @@ import * as http from "http";
 import * as http2 from "http2";
 import {Server as PipingServer} from "piping-server";
 import * as t from "io-ts";
+import * as basicAuth from "basic-auth";
+
 
 type HttpReq = http.IncomingMessage | http2.Http2ServerRequest;
 type HttpRes = http.ServerResponse | http2.Http2ServerResponse;
@@ -41,9 +43,15 @@ function createAllows(config: Config): (req: HttpReq) => boolean {
       // TODO: handle regexp
       return false;
     });
-    // TODO: handle basic auth
     return allowsPath;
   };
+}
+
+function basicAuthDenied(res: HttpRes) {
+  res.writeHead(401, {
+    'WWW-Authenticate': 'Basic realm="example"'
+  });
+  res.end("Access denied\n");
 }
 
 export function generateHandler({pipingServer, config, useHttps}: {pipingServer: PipingServer, config: Config, useHttps: boolean}): Handler {
@@ -57,6 +65,24 @@ export function generateHandler({pipingServer, config, useHttps}: {pipingServer:
       }
       throw Error('never reach');
     }
+    // Basic auth is enabled
+    if (config.basicAuthUsers !== undefined) {
+      // TODO: this type assertion may not be safe. confirm later
+      const user = basicAuth(req as http.IncomingMessage);
+      if (user === undefined) {
+        basicAuthDenied(res);
+        return;
+      }
+      const {name, pass} = user;
+      const allowsUser = config.basicAuthUsers.some(({username, password}) =>
+        username === name && password === pass
+      );
+      if (!allowsUser) {
+        basicAuthDenied(res);
+        return;
+      }
+    }
+
     pipingHandler(req, res);
   };
 }
