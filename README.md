e91aa2158248b409c8f3bed25d6487fca48cdd56
Ryo Ota <nwtgck@nwtgck.org>
2020-01-31 22:12:53 +0900

    create simple logic of hidden Piping Server

---
+++ README.md
@@ -0,0 +1,16 @@
+# hidden-piping-server
+Hidden Piping Server
+
+## Options
+
+```
+Options:
+  --help          Show help                                            [boolean]
+  --version       Show version number                                  [boolean]
+  --http-port     Port of HTTP server                            [default: 8080]
+  --enable-https  Enable HTTPS                                  [default: false]
+  --https-port    Port of HTTPS server                                  [number]
+  --key-path      Private key path                                      [string]
+  --crt-path      Certification path                                    [string]
+  --allow-path    Allow HTTP path                            [string] [required]
+```
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+// (from: https://qiita.com/takayukioda/items/a149bc2907ef77121229)
+
+import * as fs from "fs";
+import * as http from "http";
+import * as http2 from "http2";
+import * as log4js from "log4js";
+import * as yargs from "yargs";
+import * as piping from "piping-server";
+
+type HttpReq = http.IncomingMessage | http2.Http2ServerRequest;
+type HttpRes = http.ServerResponse | http2.Http2ServerResponse;
+type Handler = (req: HttpReq, res: HttpRes) => void;
+
+function generateHandler({allows, useHttps}: {useHttps: boolean, allows: (req: HttpReq) => boolean}): Handler {
+  const pipingHandler = pipingServer.generateHandler(useHttps);
+  return (req, res) => {
+    if (!allows(req)) {
+      req.socket.end();
+      return;
+    }
+    pipingHandler(req, res);
+  };
+}
+
+// Create option parser
+const parser = yargs
+  .option("http-port", {
+    describe: "Port of HTTP server",
+    default: 8080
+  })
+  .option("enable-https", {
+    describe: "Enable HTTPS",
+    default: false
+  })
+  .option("https-port", {
+    describe: "Port of HTTPS server",
+    type: "number"
+  })
+  .option("key-path", {
+    describe: "Private key path",
+    type: "string"
+  })
+  .option("crt-path", {
+    describe: "Certification path",
+    type: "string"
+  })
+  .option('allow-path', {
+    describe: "Allow HTTP path",
+    type: "string",
+    required: true,
+  });
+
+// Parse arguments
+const args = parser.parse(process.argv);
+const httpPort: number = args["http-port"];
+const enableHttps: boolean = args["enable-https"];
+const httpsPort: number | undefined = args["https-port"];
+const serverKeyPath: string | undefined = args["key-path"];
+const serverCrtPath: string | undefined = args["crt-path"];
+const allowPath: string = args["allow-path"];
+
+// Create a logger
+const logger = log4js.getLogger();
+logger.level = "info";
+
+// Create a piping server
+const pipingServer = new piping.Server(logger);
+
+const allows = (req: HttpReq) => req.url?.startsWith(allowPath) || false;
+
+http.createServer(generateHandler({allows, useHttps: false}))
+  .listen(httpPort, () => {
+    logger.info(`Listen HTTP on ${httpPort}...`);
+  });
+
+if (enableHttps && httpsPort !== undefined) {
+  if (serverKeyPath === undefined || serverCrtPath === undefined) {
+    logger.error("Error: --key-path and --crt-path should be specified");
+  } else {
+    http2.createSecureServer(
+      {
+        key: fs.readFileSync(serverKeyPath),
+        cert: fs.readFileSync(serverCrtPath),
+        allowHTTP1: true
+      },
+      generateHandler({allows, useHttps: true})
+    ).listen(httpsPort, () => {
+      logger.info(`Listen HTTPS on ${httpsPort}...`);
+    });
+  }
+}
+
+// Catch and ignore error
+process.on("uncaughtException", (err) => {
+  logger.error("on uncaughtException", err);
+});
