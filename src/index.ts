bb6f6476ce2dcb814dd7be42b2f10ac1f0d314d9
Ryo Ota <nwtgck@nwtgck.org>
2022-03-31 05:40:58 +0900

    implement "migrate-config" subcommand

--- src/index.ts
+++ src/index.ts
@@ -52,8 +52,26 @@ const parser = yargs
   .alias("config-path", "config-yaml-path")
   .command("migrate-config", "migrate an existing config to new one", (yargs) => {
   }, (argv) => {
-    // TODO: impl
-    console.log("migrate config", argv);
+    const configYaml = yaml.load(fs.readFileSync(argv.configPath, 'utf8'));
+    if (configV1Schema.safeParse(configYaml).success) {
+      console.log("The config is already a valid config v1");
+      return;
+    }
+    const configParsed = configWihtoutVersionSchema.safeParse(configYaml);
+    if(!configParsed.success) {
+      const issueStack = configParsed.error.issues.slice();
+      while (issueStack.length > 0) {
+        const issue = issueStack.pop()!;
+        if (issue.code === "invalid_union") {
+          issueStack.push(...issue.unionErrors.flatMap(e => e.issues));
+          continue;
+        }
+        process.stderr.write(`config error hint: ${JSON.stringify(issue)}\n`);
+      }
+      process.exit(1);
+    }
+    const configV1 = migrateToConfigV1(configParsed.data);
+    console.log(yaml.dump(configV1));
   });
 
 
